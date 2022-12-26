8cbca4a6d3b38eaa7a92391a4ac137fd2729a8d7
Mogan <106959238+Mrdotov@users.noreply.github.com>
2022-12-26 23:44:00 +0200

    add data-num in html and add click check

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const buttons = document.querySelectorAll('.number')
-console.log(buttons);
+const options = document.querySelectorAll('.options')
+
+options.forEach(option => {
+    option.addEventListener('click', function() {
+        console.log(event.target);
+    })
+})
+
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        console.log(event.target)
+    })
+})
 
 // document.getElementById('output').innerHTML + result;
