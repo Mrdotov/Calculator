3969f5b569b8a1f4773f08670cbd997e16540790
Mogan <106959238+Mrdotov@users.noreply.github.com>
2022-12-27 11:22:16 +0200

    Add fix(display numbers in input)

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
-const buttons = document.querySelectorAll('.number')
-const options = document.querySelectorAll('.options')
+// value - получить текст из input
+// innerHTML - записать текст в правый тег
+// . - для класса
+// # для id
 
+const buttons = document.querySelectorAll('.number');
+const options = document.querySelectorAll('.options');
+const input = document.querySelector('#output');
+let firstNumber = 0;
+let operation = null;
+
+function out() {
+    buttons.values === input.values
+}
+//Проверка на роботу кнопок
 options.forEach(option => {
     option.addEventListener('click', function() {
-        console.log(event.target.dataset.num);
-    })
-})
-
+        firstNumber = Number.parseInt(output.value)
+        operation = event.currentTarget.dataset.action;
+    });
+});
 buttons.forEach(button => {
     button.addEventListener('click', function() {
-        console.log(event.target.dataset.num);
-    })
+
+        let value = event.currentTarget.textContent
+        if (true) {
+            output.value = value;
+        } else {
+            output.value += value;
+        }
+    });
+});
+
+const equal = document.querySelector('.equal')
+
+equal.addEventListener('click', () => {
+    if (!options) {
+        return
+    }
+
 })
+
+
+
 
 // document.getElementById('output').innerHTML + result;
