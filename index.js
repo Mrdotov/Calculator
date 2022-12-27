ed4f02c3b051346b4e80163af3c2b8530b6d1c7e
Mogan <106959238+Mrdotov@users.noreply.github.com>
2022-12-27 11:57:56 +0200

    output in input

--- index.js
+++ index.js
@@ -6,17 +6,15 @@
 const buttons = document.querySelectorAll('.number');
 const options = document.querySelectorAll('.options');
 const input = document.querySelector('#output');
+const equal = document.querySelector('.equal');
 let firstNumber = 0;
-let operation = null;
+let secondNumber = Number.parseInt(output.value)
 
-function out() {
-    buttons.values === input.values
-}
 //Проверка на роботу кнопок
 options.forEach(option => {
     option.addEventListener('click', function() {
         firstNumber = Number.parseInt(output.value)
-        operation = event.currentTarget.dataset.action;
+        option = event.currentTarget.dataset.action;
     });
 });
 buttons.forEach(button => {
@@ -31,16 +29,24 @@ buttons.forEach(button => {
     });
 });
 
-const equal = document.querySelector('.equal')
-
-equal.addEventListener('click', () => {
+equal.addEventListener('click', function() {
     if (!options) {
-        return
+        return;
     }
-
+    if (options === 'subtract') {
+        output.value = substract(firstNumber, secondNumber);
+    }
+    if (options === 'multiply') {
+        output.value = firstNumber + secondNumber;
+    }
+    if (options === 'substruct') {
+        output.value = firstNumber + secondNumber;
+    }
+    if (options === 'sum') {
+        output.value = firstNumber + secondNumber;
+    }
 })
-
-
+console.log(equal);
 
 
 // document.getElementById('output').innerHTML + result;
